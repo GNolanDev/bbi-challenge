e0b729074a64f75dd589ef9ce46c394c4d873809
GNolanDev <GNolanDev@gmail.com>
2022-03-12 22:55:28 +0000

    Compiled example page separately - working on the main app.js only from now on

--- resources/views/testpage.blade.php
+++ resources/views/testpage.blade.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Page</title>
-    <script defer src="/js/manifest.js"></script>
-    <script defer src="/js/vendor.js"></script>
-    <script defer src="/js/app.js"></script>
+    <script defer src="/example/js/manifest.js"></script>
+    <script defer src="/example/js/vendor.js"></script>
+    <script defer src="/example/js/example.js"></script>
 </head>
 
 <body>
